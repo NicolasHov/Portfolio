42338b854f4a5fece20686132c90dc87be06867d
Nicolas Hovart <hovart.nicolas@gmail.com>
2021-03-24 08:47:22 +0100

    Rename App.css to App.scss

--- src/App.js
+++ src/App.js
@@ -1,76 +1,117 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <article id="02cbecdb-5efd-400e-9409-ec61d0c66e72" class="page sans">
+      <article id="02cbecdb-5efd-400e-9409-ec61d0c66e72" className="page sans">
         <header>
-          <h1 class="page-title">Nicolas Hovart</h1>
+          <h1 className="page-title">Nicolas Hovart</h1>
         </header>
-        <div class="page-body">
-          <p id="2fa7d292-6261-4409-92c9-4624693def0d" class="">Développeur React Web&amp;Mobile</p>
-          <h3 id="b6fdbcd0-ee27-437a-af45-fb2da3ac8040" class="">Développeur React Web&amp;Mobile</h3>
-          <p id="3986a0c1-018d-4885-873e-082b8188ebd9" class=""><strong>Paris</strong></p>
-          <figure id="01ecd5d2-5b19-4c9f-a95c-b237d0d20cfa" class="image">
+        <div className="page-body">
+          <p id="2fa7d292-6261-4409-92c9-4624693def0d" className="">Développeur React Web&amp;Mobile</p>
+          <h3 id="b6fdbcd0-ee27-437a-af45-fb2da3ac8040" className="">Développeur React Web&amp;Mobile</h3>
+          <p id="3986a0c1-018d-4885-873e-082b8188ebd9" className=""><strong>Paris</strong></p>
+          <figure id="01ecd5d2-5b19-4c9f-a95c-b237d0d20cfa" className="image">
             <a
               href="https://process.filestackapi.com/output=format:png/negative/modulate=brightness:1000/compress/KKn2Q218TaeF51faVayy">
-              <img src="https://process.filestackapi.com/output=format:png/negative/modulate=brightness:1000/compress/KKn2Q218TaeF51faVayy" alt=""/>
+              <img src="https://process.filestackapi.com/output=format:png/negative/modulate=brightness:1000/compress/KKn2Q218TaeF51faVayy" alt="" />
             </a>
           </figure>
-          <h2 id="122069f8-a97f-4b12-9ff2-5c9de836650e" class="">Je code avec React</h2>
-          <p id="8422e563-7ffe-488b-85d5-e05e15255500" class=""></p>
-          <h3 id="458b33b6-8b1a-4516-bc45-e80526cd374c" class="">Localisation</h3>
-          <p id="6ea727d9-38fe-409a-9da1-352b3204327c" class="">J&#x27;aimerais faire du télétravail régulier (quelque
-				fois par semaine)</p>
-          <p id="83bf70b5-c5c2-4972-a5ed-09c90e73c0fa" class=""><strong>Contrat</strong>Cdd, Freelance ou CDI</p>
-          <h3 id="fc4877d9-eef0-44bc-b4d7-c78f70e2fd6c" class="">J&#x27;aimerais en particulier</h3>
-          <ul id="699fbc86-d107-4469-8d77-bc7e3b61688a" class="bulleted-list">
-            <li>Du challenge</li>
-          </ul>
-          <ul id="2285dfec-55a5-4753-b0c3-199f9621c7ca" class="bulleted-list">
-            <li>Une bonne ambiance</li>
-          </ul>
-          <ul id="298d8c2c-da36-42e0-8769-7d127e86cfa0" class="bulleted-list">
-            <li>Autre</li>
-          </ul>
-          <ul id="0822a0a1-c3cb-4241-ac6b-ca80da1455af" class="bulleted-list">
-            <li>Des responsabilités</li>
-          </ul>
-          <ul id="18009aad-dcdf-4a84-b4a8-feddaf8b2e94" class="bulleted-list">
-            <li>Le salaire</li>
-          </ul>
-          <ul id="2e743a1d-fe7c-4d68-bcf6-adb2b18a238a" class="bulleted-list">
-            <li>Des formations</li>
-          </ul>
-          <h2 id="2c38ab25-87ec-4d2f-ba9b-d02993ffaadd" class="">J&#x27;aimerais travailler à Paris, France (+ 1)</h2>
-          <figure id="151f1dcd-9558-4911-ae35-5b9a0a91bffa" class="image"><a
-            href="https://cdn.filestackcontent.com/resize=w:500/compress/dLXE4WC8S3mZFZNfClTK">
-            <img src="https://cdn.filestackcontent.com/resize=w:500/compress/dLXE4WC8S3mZFZNfClTK" alt=""/></a>
-          </figure>
-          <h2 id="8c692a96-ad61-4d35-b0a7-48b65f98ef15" class="">Mon projet coup de coeur : Instant Visio</h2>
-          <h2 id="44933b10-fd76-491f-967a-ce571334ee3b" class="">Développeur React Native &amp; React@Newtype()</h2>
-          <h2 id="f50b206c-d0b5-4ddd-9375-358ed45e8b5b" class="">I gratuated from Université Paris Descartes</h2>
-          <figure id="96c0dd7c-e559-4f20-9fdb-defbc305de5a" class="image">
-            <a
-              href="https://media3.giphy.com/media/l41JU9pUyosHzWyuQ/giphy.gif?cid=f7fb5bbafu3d1x4yqehu2bzmzzfkea78qwg9f814xcx7b3px&amp;rid=giphy.gif">
-              <img src="https://media3.giphy.com/media/l41JU9pUyosHzWyuQ/giphy.gif?cid=f7fb5bbafu3d1x4yqehu2bzmzzfkea78qwg9f814xcx7b3px&amp;rid=giphy.gif" alt="" />
-            </a>
-          </figure>
-          <p id="575d1574-73ba-4e39-bc4a-41da333d3904" class=""><strong>Hobbies</strong></p>
-          <p id="d82bf18f-b63a-46e9-8a93-963e6003c93a" class="">
-            <a href="https://giphy.com/adultswim/">Adult Swim</a>
-          </p>
-          <figure id="53dc725a-af51-4d46-a713-cefaca2e4eea" class="image">
-            <a href="https://media2.giphy.com/avatars/adultswim/8hITsesscHXE.jpg">
-              <img src="https://media2.giphy.com/avatars/adultswim/8hITsesscHXE.jpg" alt=""/>
-            </a>
-          </figure>
-          <h2 id="44cd4bf0-92e9-45e0-9cb2-98b0312320e5" class="">Intéressé par :</h2>
-          <h2 id="706cef3e-80d4-4637-82d5-a06d6573d96c" class="">Je parle FrançaisAnglais</h2>
-          <h2 id="08f50894-bd11-47b6-a057-d5e2529b049a" class="">Découvrez ma tracklist</h2>
+          <div className="columns">
+            <div className="column">
+              <h2 id="122069f8-a97f-4b12-9ff2-5c9de836650e" className="">Je code avec React</h2>
+              <p id="8422e563-7ffe-488b-85d5-e05e15255500" className=""></p>
+              <h3 id="458b33b6-8b1a-4516-bc45-e80526cd374c" className="">Localisation</h3>
+              <p id="6ea727d9-38fe-409a-9da1-352b3204327c" className="">J&#x27;aimerais faire du télétravail régulier (quelque
+          fois par semaine)</p>
+              <p id="83bf70b5-c5c2-4972-a5ed-09c90e73c0fa" className=""><strong>Contrat</strong>Cdd, Freelance ou CDI</p>
+              <h3 id="fc4877d9-eef0-44bc-b4d7-c78f70e2fd6c" className="">J&#x27;aimerais en particulier</h3>
+              <ul id="699fbc86-d107-4469-8d77-bc7e3b61688a" className="bulleted-list">
+                <li>Du challenge</li>
+              </ul>
+              <ul id="2285dfec-55a5-4753-b0c3-199f9621c7ca" className="bulleted-list">
+                <li>Une bonne ambiance</li>
+              </ul>
+              <ul id="298d8c2c-da36-42e0-8769-7d127e86cfa0" className="bulleted-list">
+                <li>Autre</li>
+              </ul>
+              <ul id="0822a0a1-c3cb-4241-ac6b-ca80da1455af" className="bulleted-list">
+                <li>Des responsabilités</li>
+              </ul>
+              <ul id="18009aad-dcdf-4a84-b4a8-feddaf8b2e94" className="bulleted-list">
+                <li>Le salaire</li>
+              </ul>
+              <ul id="2e743a1d-fe7c-4d68-bcf6-adb2b18a238a" className="bulleted-list">
+                <li>Des formations</li>
+              </ul>
+            </div>
+            <div className="column">
+              <h2 id="2c38ab25-87ec-4d2f-ba9b-d02993ffaadd" className="">J&#x27;aimerais travailler à Paris, France (+ 1)</h2>
+              <figure id="151f1dcd-9558-4911-ae35-5b9a0a91bffa" className="image"><a
+                href="https://cdn.filestackcontent.com/resize=w:500/compress/dLXE4WC8S3mZFZNfClTK">
+                <img src="https://cdn.filestackcontent.com/resize=w:500/compress/dLXE4WC8S3mZFZNfClTK" alt="" /></a>
+              </figure>
+              <h2 id="8c692a96-ad61-4d35-b0a7-48b65f98ef15" className="">Mon projet coup de coeur : Instant Visio</h2>
+              <h2 id="44933b10-fd76-491f-967a-ce571334ee3b" className="">Développeur React Native &amp; React@Newtype()</h2>
+              <h2 id="f50b206c-d0b5-4ddd-9375-358ed45e8b5b" className="">I gratuated from Université Paris Descartes</h2>
+              <figure id="96c0dd7c-e559-4f20-9fdb-defbc305de5a" className="image">
+                <a
+                  href="https://media3.giphy.com/media/l41JU9pUyosHzWyuQ/giphy.gif?cid=f7fb5bbafu3d1x4yqehu2bzmzzfkea78qwg9f814xcx7b3px&amp;rid=giphy.gif">
+                  <img src="https://media3.giphy.com/media/l41JU9pUyosHzWyuQ/giphy.gif?cid=f7fb5bbafu3d1x4yqehu2bzmzzfkea78qwg9f814xcx7b3px&amp;rid=giphy.gif" alt="" />
+                </a>
+              </figure>
+              <p id="575d1574-73ba-4e39-bc4a-41da333d3904" className=""><strong>Hobbies</strong></p>
+              <p id="d82bf18f-b63a-46e9-8a93-963e6003c93a" className="">
+                <a href="https://giphy.com/adultswim/">Adult Swim</a>
+              </p>
+              <figure id="53dc725a-af51-4d46-a713-cefaca2e4eea" className="image">
+                <a href="https://media2.giphy.com/avatars/adultswim/8hITsesscHXE.jpg">
+                  <img src="https://media2.giphy.com/avatars/adultswim/8hITsesscHXE.jpg" alt="" />
+                </a>
+              </figure>
+              <h2 id="44cd4bf0-92e9-45e0-9cb2-98b0312320e5" className="">Intéressé par :</h2>
+              <h2 id="706cef3e-80d4-4637-82d5-a06d6573d96c" className="">Je parle FrançaisAnglais</h2>
+              <h2 id="08f50894-bd11-47b6-a057-d5e2529b049a" className="">Découvrez ma tracklist</h2>
+
+
+              <p className="subtitle">
+                Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+              </p>
+              <div className="field">
+                <div className="control">
+                  <input className="input" type="text" placeholder="Input" />
+                  <input type="checkbox" />
+                </div>
+              </div>
+
+              <div className="field">
+                <p className="control">
+                  <span className="select">
+                    <select>
+                      <option>Select dropdown</option>
+                    </select>
+                  </span>
+                </p>
+              </div>
+              <div className="field">
+                <p className="control">
+                  <span className="select">
+                    <select>
+                      <option>Select dropdown</option>
+                    </select>
+                  </span>
+                </p>
+              </div>
+              <div className="buttons">
+                <a className="button is-primary" href="#">Primary</a>
+                <a className="button is-link" href="#">Link</a>
+              </div>
+            </div>
+          </div>
         </div>
       </article>
     </div>
+
   );
 }
 
